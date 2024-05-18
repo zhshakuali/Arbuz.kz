3622c9d696e82ed4529239a932f27895f0677aa8
zhshakuali <zhansayashakuali200309@gmail.com>
2024-05-18 13:22:34 +0500

    added main screen

--- ArbuzKZ/MainTabBarViewController.swift
+++ ArbuzKZ/MainTabBarViewController.swift
@@ -11,17 +11,19 @@ import SwiftUI
 class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
-           super.viewDidLoad()
-           
-           let mainViewController = UIHostingController(rootView: MainView())
-           mainViewController.tabBarItem = UITabBarItem(title: "Главная", image: UIImage(systemName: "house"), tag: 0)
-           
-           let cartViewController = BasketViewController()
-           cartViewController.tabBarItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "cart"), tag: 1)
-           
-           viewControllers = [mainViewController, cartViewController]
-           
-           tabBar.tintColor = .green
-       }
-
+        super.viewDidLoad()
+        
+        addViewControllers()
+        tabBar.tintColor = .green
+    }
+    
+    private func addViewControllers() {
+        let mainViewController = MainScreenViewController()
+        mainViewController.tabBarItem = UITabBarItem(title: "Главная", image: UIImage(systemName: "house"), tag: 0)
+        
+        let cartViewController = BasketViewController()
+        cartViewController.tabBarItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "cart"), tag: 1)
+        
+        viewControllers = [mainViewController, cartViewController]
+    }
 }
